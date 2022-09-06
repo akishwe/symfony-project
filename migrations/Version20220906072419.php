6d7db31ab3b0aea650e50a65c848e0505ab29f26
akishwe <avinashkishwe1@gmail.com>
2022-09-06 12:57:42 +0530

    Added Category Fixtures and Data Fixtures Package

--- migrations/Version20220906072419.php
+++ migrations/Version20220906072419.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220905124806 extends AbstractMigration
+final class Version20220906072419 extends AbstractMigration
 {
     public function getDescription(): string
     {
